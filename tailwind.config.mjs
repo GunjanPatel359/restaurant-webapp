6e6f99450ab50655048b6af5dc25c9943d69b5d3
gunjan patel <g.a.patel359@gmail.com>
2025-01-04 17:30:25 +0530

    Initial commit from Create Next App

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        coloralpha:"var(--color-alpha)",
+        color0:"var(--color-0)",
+        color1:"var(--color-1)",
+        color2:"var(--color-2)",
+        color3:"var(--color-3)",
+        color4:"var(--color-4)",
+        color5:"var(--color-5)",
+      },
+    },
+  },
+  plugins: [],
+};
